3a9995482149aebefc7ba69d3a4fb0e1c4ac80d6
Ladoo <souravkumar54325@gmail.com>
2018-12-07 09:13:37 +0530

    new

--- app/src/main/java/com/errorfoundtechnologies/eft/MainActivity.kt
+++ app/src/main/java/com/errorfoundtechnologies/eft/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.errorfoundtechnologies.eft
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        b1.setOnClickListener {
+            var i = Intent(this@MainActivity, Main2Activity::class.java)
+            startActivity(i)
+        }
+        cd1.setOnClickListener {
+            var i = Intent(this@MainActivity, Main2Activity::class.java)
+            startActivity(i)
+        }
 
     }
 }
